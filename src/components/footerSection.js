3def1bd306083225505553f2f6b5a0393fe64dbc
KunJoha <37103381+KunJoha@users.noreply.github.com>
2019-12-16 09:29:17 +0100

    things

--- src/components/footerSection.js
+++ src/components/footerSection.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 function FooterSection() {
   return (
     <Fragment>
-      <p>
+      <p className="text-right">
         <Link to="/Kontakt">Kontakt</Link>
       </p>
-      <p>
+      <p className="text-center">
         <Link to="/Impressum">Impressum</Link>
       </p>
-      <p>
+      <p className="text-center">
         <Link to="/UeberUns">Über uns</Link>
       </p>
-      <p>
+      <p className="text-left">
         <Link to="/Datenschutz">Datenschutz</Link>
       </p>
     </Fragment>
